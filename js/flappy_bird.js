a293e8fed5c4badca5b54ec552d84bed844b1cb1
MK <wushihao2012@163.com>
2017-04-13 01:01:51 +0800

    改

--- js/flappy_bird.js
+++ js/flappy_bird.js
@@ -1,31 +1,31 @@
 /*
-* 该对象负责直接调度几个角色对象，并提供如下几个方法：
-*
-* run: 对外开发的游戏开始方法
-*
-* draw: 绘制游戏画面
-*
-* start: 开始运行游戏
-*
-* end: 结束游戏
-*
-* pause：暂停继续游戏
-*
-* restart: 重新开始游戏
-*
-* */
+ * 该对象负责直接调度几个角色对象，并提供如下几个方法：
+ *
+ * run: 对外开发的游戏开始方法
+ *
+ * draw: 绘制游戏画面
+ *
+ * start: 开始运行游戏
+ *
+ * end: 结束游戏
+ *
+ * pause：暂停继续游戏
+ *
+ * restart: 重新开始游戏
+ *
+ * */
 
 /*
-* @constructor { Function }  游戏主对象
-* @param { container: string } 容器选择器
-* */
+ * @constructor { Function }  游戏主对象
+ * @param { container: string } 容器选择器
+ * */
 function FlappyBird(container) {
     /*
-    * 1、调用util里的getCtx方法，动态创建画布与绘制上下文
-    * 2、利用imgLoad对象加载游戏所需的图像资源
-    * 3、当图像资源加载完毕后，创建所有的角色对象
-    * 4、创建好之后，就可以start了。
-    * */
+     * 1、调用util里的getCtx方法，动态创建画布与绘制上下文
+     * 2、利用imgLoad对象加载游戏所需的图像资源
+     * 3、当图像资源加载完毕后，创建所有的角色对象
+     * 4、创建好之后，就可以start了。
+     * */
     this.ctx = util.getCtx(800, 600, container);
     this.isPause = false;
     this.bind();
@@ -49,20 +49,21 @@ FlappyBird.prototype = {
     },
 
     /*
-    * @根据角色占用宽度计算轮播时需要的数量
-    * @param { width: Number } 角色占用宽度
-    * @return 所需个数
-    * */
+     * @根据角色占用宽度计算轮播时需要的数量
+     * @param { width: Number } 角色占用宽度
+     * @return 所需个数
+     * */
     getRoleNumber: function(width) {
         return Math.ceil(this.ctx.canvas.width / width) + 1;
     },
 
     /*
-    * @创建游戏所需的角色
-    * @param { imgs: Object } 所有已加载完毕的图像资源
-    * */
+     * @创建游戏所需的角色
+     * @param { imgs: Object } 所有已加载完毕的图像资源
+     * */
     initRoles: function(imgs) {
-        var tempTotle = 0, i;
+        var tempTotle = 0,
+            i;
 
         this.roles = {
             sky: [],
@@ -81,7 +82,7 @@ FlappyBird.prototype = {
 
         // 计算所需大地数量，然后创建
         tempTotle = this.getRoleNumber(imgs.land.width);
-        for(i = 0; i < tempTotle; i++) {
+        for (i = 0; i < tempTotle; i++) {
             this.roles.land.push(new Land({
                 ctx: this.ctx,
                 img: imgs.land
@@ -90,7 +91,7 @@ FlappyBird.prototype = {
 
         // 管道
         tempTotle = this.getRoleNumber(imgs.pipeDown.width + 120);
-        for(i = 0; i < tempTotle; i++) {
+        for (i = 0; i < tempTotle; i++) {
             this.roles.pipe.push(new Pipe({
                 ctx: this.ctx,
                 img: {
@@ -105,7 +106,7 @@ FlappyBird.prototype = {
 
         // 计算所需背景数量，然后创建
         tempTotle = this.getRoleNumber(imgs.sky.width);
-        for(i = 0; i < tempTotle; i++) {
+        for (i = 0; i < tempTotle; i++) {
             this.roles.sky.push(new Sky({
                 ctx: this.ctx,
                 img: imgs.sky
@@ -121,12 +122,12 @@ FlappyBird.prototype = {
     // 绘制游戏画面
     _draw: function() {
         /*
-        * 绘制所有的角色：
-        * 1、遍历this.roles，取出每一种角色数组
-        * 2、在遍历每一种角色数组，得到单个角色对象，调用其draw方法进行绘制
-        * */
+         * 绘制所有的角色：
+         * 1、遍历this.roles，取出每一种角色数组
+         * 2、在遍历每一种角色数组，得到单个角色对象，调用其draw方法进行绘制
+         * */
         var key;
-        for(key in this.roles) {
+        for (key in this.roles) {
             this.roles[key].forEach(function(role) {
                 role.draw();
             });
@@ -136,25 +137,25 @@ FlappyBird.prototype = {
     // 绘制游戏画面
     draw: function() {
         /*
-        * 1、开启定时器
-        * 2、清除当前游戏画面
-        * 3、遍历得到所有的角色依次更新数据与绘制
-        * 3.1、遍历this.roles，取出每一种角色数组
-        * 3.2、在遍历每一种角色数组，得到单个角色对象
-        * 3.3、调用其update方法更新数据，调用draw方法进行绘制
-        * */
+         * 1、开启定时器
+         * 2、清除当前游戏画面
+         * 3、遍历得到所有的角色依次更新数据与绘制
+         * 3.1、遍历this.roles，取出每一种角色数组
+         * 3.2、在遍历每一种角色数组，得到单个角色对象
+         * 3.3、调用其update方法更新数据，调用draw方法进行绘制
+         * */
         var key, self = this;
         setInterval(function() {
 
             // 没有暂停才进行角色绘制
-            if(!self.isPause) {
+            if (!self.isPause) {
 
                 // 清除路径与画布渲染内容
                 self.ctx.beginPath();
                 self.ctx.clearRect(0, 0, self.ctx.canvas.width, self.ctx.canvas.height);
 
                 // 绘制所有角色
-                for(key in self.roles) {
+                for (key in self.roles) {
                     self.roles[key].forEach(function(role) {
                         role.update(role.constructor.len);
                         role.draw();
@@ -162,7 +163,7 @@ FlappyBird.prototype = {
                 }
 
                 // 判断小鸟是否死亡，死亡则结束游戏
-                if(self.isDie()) {
+                if (self.isDie()) {
                     self.end();
                 }
             }
@@ -176,7 +177,7 @@ FlappyBird.prototype = {
         var birdCoreY = this.roles.bird[0].y + this.roles.bird[0].height / 2;
 
         // 小鸟中心点碰撞到了管道
-        if(this.ctx.isPointInPath(birdCoreX, birdCoreY)) {
+        if (this.ctx.isPointInPath(birdCoreX, birdCoreY)) {
             return true;
         }
 
@@ -207,6 +208,9 @@ FlappyBird.prototype = {
     bind: function() {
         var self = this;
         this.ctx.canvas.addEventListener('click', function() {
+            self.roles.bird[0].flyup();
+        });
+        this.ctx.canvas.addEventListener('touch', function() {
             self.roles.bird[0].flyup();
         });
     }
